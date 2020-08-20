4458fbd9515a40b51ae2f8bef10a47810709096e
Adrian <revanskyw@gmail.com>
2020-08-19 20:55:06 -0400

    se agrega iteracion de datos

--- src/Rickandmorty.js
+++ src/Rickandmorty.js
@@ -1,9 +1,40 @@
 import axios from "axios";
 
+class Personajes{
+constructor(nombre,id,genero,edad,especies,estado,imagen){
+
+    this.nombre = nombre
+    this.id = id
+    this.genero = genero
+    this.edad = edad
+    this.especies = especies
+    this.estado = estado
+
+}
+
+
+
+}
+
+
+
+let personaje = new Personajes()
+
+
 const rickMorty = async () => {
     try {
        const baseUrl = await axios.get("https://rickandmortyapi.com/api/character");
-        
+        let datos = baseUrl.data.results
+let personajes= []
+        datos.forEach(e => {
+            
+        personajes.push(new Personajes(e.name,e.id,e.gender,e.age,e.species,e.status,e.image))
+
+
+        });
+        console.log(personajes)
+
+
     } catch (error) {
         
     }
@@ -15,4 +46,4 @@ const rickMorty = async () => {
 
 
 
-export default rickMorty
+export default 
