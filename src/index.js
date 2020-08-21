c32495382047f57ccbab326ce440e5c76b2ddf3c
Viviana <vivi_reyes_c@hotmail.com>
2020-08-20 23:11:49 -0300

    jquery

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 //  Bienvenidos al nuevo Hello World! programadores!
 import rickMorty from './Rickandmorty'
+import $ from 'jquery'
+
+
+
 
 rickMorty.then((personajes) => {
     const person  = document.getElementById('personajes')
     personajes.forEach(element => {
-        
         person.innerHTML +=  `
         
     <div class="card mb-3" style="max-width: 540px;">
     <div class="row no-gutters">
       <div class="col-md-4">
-        <img src="${element.imagen}" class="card-img" alt="image">
+        <img id="${element.id}" src="${element.imagen}" class="card-img imagen" alt="image">
       </div>
       <div class="col-md-8">
-        <div class="card-body">
+        <div class="card-body ${element.id}" ">
           <h5 class="card-title">${element.nombre}</h5>
           <p class="card-text">${element.genero}</p>
           <p class="card-text"><small class="text-muted">${element.especies}</small></p>
@@ -23,6 +26,10 @@ rickMorty.then((personajes) => {
     </div>
   </div>  `
         
+    });
+    
+    $('.imagen').click((e)=>{
+       $(`.${e.target.id}`).css('display', 'block')
     });
     console.log(personajes)
 
