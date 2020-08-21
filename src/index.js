f097414c96624c937a8b38eb3bacbd45a276d960
Giovanni <giodirosaartes@gmail.com>
2020-08-20 21:02:43 -0400

    style card print dom character bootstrap

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ rickMorty.then((personajes) => {
     const person  = document.getElementById('personajes')
     personajes.forEach(element => {
         
-        person.innerHTML +=  `<div class="personajes">
-        <img src="${element.imagen}" width="100"> 
-        <ul>
-        <li>${element.nombre}</li>
-        <li>${element.especies}</li>
-        <li>${element.genero}</li>
-        <li>${element.estado}</li> 
-      </ul>
-        </div>  `
+        person.innerHTML +=  `
+        
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img src="${element.imagen}" class="card-img" alt="image">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${element.nombre}</h5>
+          <p class="card-text">${element.genero}</p>
+          <p class="card-text"><small class="text-muted">${element.especies}</small></p>
+          <p class="card-text"><small class="text-muted">${element.estado}</small></p>
+        </div>
+      </div>
+    </div>
+  </div>  `
         
     });
     console.log(personajes)
